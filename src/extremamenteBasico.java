8dae08eaf3b0f159443e8930060d92d9a03e5dcf
Luan Matos <luanmatos.ti@gmail.com>
2023-12-30 23:07:17 -0300

    Adicionando exercicíos 1000 e 1001.

---
+++ src/extremamenteBasico.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class extremamenteBasico {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int X = A+B;
+
+        System.out.println("X = " + X);
+    }
+}
